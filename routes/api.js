fa06d4dcfcaacf0351ef91bae35c1ae48f6a408a
Josef Vacek <vacekj@outlook.com>
2019-09-02 11:30:11 +0200

    fix: error and no prijmeni provided

--- routes/api.js
+++ routes/api.js
@@ -12,6 +12,10 @@ router.get('/', function (req, res) {
 
 
 router.post('/ucitel/', async function (req, res) {
+	const prijmeniBody = req.body["prijmeni"];
+	if (prijmeniBody) {
+
+	}
 	const prijmeni = diacritics.remove(req.body["prijmeni"]).toLowerCase();
 	const photoUrl = getPhotoUrl(prijmeni);
 	return res.json(sendPhoto(photoUrl));
@@ -34,6 +38,10 @@ function sendPhoto(url) {
 			}
 		]
 	};
+}
+
+function sendMessage(message) {
+
 }
 
 module.exports = router;
